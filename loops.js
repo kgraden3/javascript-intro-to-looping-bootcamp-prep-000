ebe61a5934680e15b51a8f297e1b7b1cc8fc505a
Kyle Graden <kylegraden3@gmail.com>
2019-02-13 18:04:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -30,5 +30,5 @@ function incrementVariable() {
 function doWhileLoop(n) {
   do {
     console.log('I run once regardless.');
-  } while (incrementVariable() =< n);
+  } while (incrementVariable() <= n);
 }
